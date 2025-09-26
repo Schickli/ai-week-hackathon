0fdffe9abf41adb95344beee6dc56fab866c55dd
maurin schickli <maurin.schickli@buhlergroup.com>
2025-09-26 13:08:04 +0200

    Generate Embeddings

--- source/services/ai-agent.ts
+++ source/services/ai-agent.ts
@@ -1,17 +1,61 @@
-import { AssistantModelMessage, generateText, SystemModelMessage, ToolModelMessage, UserModelMessage } from "ai";
-import { createAzure } from '@ai-sdk/azure';
+import {
+  AssistantModelMessage,
+  embed,
+  generateText,
+  SystemModelMessage,
+  ToolModelMessage,
+  UserModelMessage,
+} from "ai";
+import { createAzure } from "@ai-sdk/azure";
 
 const azure = createAzure();
 
-export async function promptAI(messages: Array<SystemModelMessage | UserModelMessage | AssistantModelMessage | ToolModelMessage>) {
+export async function promptAI(
+  messages: Array<
+    | SystemModelMessage
+    | UserModelMessage
+    | AssistantModelMessage
+    | ToolModelMessage
+  >
+) {
   const { text } = await generateText({
     model: azure("gpt-5"),
-    system: systemPrompt,
-    messages: messages
+    system: systemPromptEstimation,
+    messages: messages,
   });
 
   return text;
 }
 
-const systemPrompt = `
+export async function generateEmbeddings(text: string) {
+  const { embedding } = await embed({
+    model: azure.textEmbeddingModel("text-embedding-3-large"),
+    value: text,
+  });
+
+  return embedding;
+}
+
+export async function generateCaseDescriptionForEmbeddings(
+  messages: Array<
+    | SystemModelMessage
+    | UserModelMessage
+    | AssistantModelMessage
+    | ToolModelMessage
+  >
+) {
+  const { text } = await generateText({
+    model: azure("gpt-5"),
+    system: systemPromptImageDescription,
+    messages: messages,
+  });
+
+  return text;
+}
+
+const systemPromptEstimation = `
 TODO`;
+
+const systemPromptImageDescription = `
+TODO
+`;
